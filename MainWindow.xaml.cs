7bd758ad204b58cb035d5fb04058f859abe12c2d
Владислав Гречанник <m2108502@edu.misis.ru>
2022-05-09 21:31:06 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.Xml;
+using System.Text;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabaOOP7_8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadRss(object sender, RoutedEventArgs e)
+        {
+            var RSSString = RSSLink.Text;
+            try
+            {
+                var RSSXML = WebRequest.Create(RSSString);
+                using var response = RSSXML.GetResponse();
+                using var stream = response.GetResponseStream();
+                using var reader = new StreamReader(stream);
+                var RSSXMLString = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+                response.Close();
+                Raw.Document.Blocks.Clear();
+                Raw.AppendText(RSSXMLString);
+                var RSSXMLDocument = new XmlDocument();
+                RSSXMLDocument.LoadXml(RSSXMLString);
+                var channelNode = RSSXMLDocument.SelectSingleNode("rss/channel").Unwrap();
+                var RSSNews = new RSSArticle(channelNode);
+                Parsed.Document.Blocks.Clear();
+                Parsed.AppendText(RSSNews.ToString());
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("Невозможно подключиться к сайту");
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("Данные на сайте не являются валидным XML");
+            }
+            catch
+            {
+                MessageBox.Show("Неизвестная ошибка");
+            }
+        }
+    }
+}
---
+++ RSSItem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml;
+
+#nullable enable
+
+namespace LabaOOP7_8
+{
+    internal class RSSItem
+    {
+        readonly string? Title;
+        readonly Uri? Link;
+        readonly string? Description;
+        readonly string? Author;
+        readonly string? Date;
+
+        public RSSItem(XmlNode node)
+        {
+            Title = node.SelectSingleNode("title")?.InnerText;
+            var linkNode = node.SelectSingleNode("link");
+            if (linkNode != null) Link = new Uri(linkNode.InnerText);
+            Description = node.SelectSingleNode("description")?.InnerText;
+            Author = node.SelectSingleNode("author")?.InnerText;
+            Date = node.SelectSingleNode("pubDate")?.InnerText;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder(58);
+            // Check each property if it is null
+            // If not, add it
+            if (Title != null) sb.AppendLine($"Заголовок - {Title}");
+            if (Link != null) sb.AppendLine($"Ссылка - {Link}");
+            if (Description != null) sb.AppendLine($"Описание - {Description}");
+            if (Author != null) sb.AppendLine($"Автора - {Author}");
+            if (Date != null) sb.AppendLine($"Дата - {Date}");
+
+            return sb.ToString();
+        }
+    }
+
+    class RSSArticle
+    {
+        readonly string Title;
+        readonly Uri Link;
+        readonly string Description;
+        readonly List<RSSItem> Items;
+
+        public RSSArticle(XmlNode node)
+        {
+            Title = node.SelectSingleNode("title").Unwrap().InnerText;
+            Link = new Uri(node.SelectSingleNode("link").Unwrap().InnerText);
+            Description = node.SelectSingleNode("description").Unwrap().InnerText;
+            var items = node.SelectNodes("item");
+            Items = new List<RSSItem>(items.Count);
+            foreach (XmlNode item in items)
+                Items.Add(new RSSItem(item));
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder(54);
+            sb.AppendLine($"Название канала - {Title}");
+            sb.AppendLine($"Ссылка на канал - {Link}");
+            sb.AppendLine($"Описание канала - {Description}");
+            foreach (var item in Items)
+            {
+                sb.AppendLine(new string('=', 40));
+                sb.AppendLine(item.ToString().TrimEnd());
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    static class Extensions
+    {
+        public static T Unwrap<T>(this T? item)
+        where T: class
+        {
+            if (item is null)
+                throw new ArgumentNullException(nameof(item));
+            return item;
+        }
+    }
+}
